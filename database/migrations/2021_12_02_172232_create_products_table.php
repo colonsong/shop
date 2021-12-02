cddc1208c4213ba6c47bf1cf826754211e514886
colin <colon.song@gmail.com>
2021-12-03 01:26:56 +0800

    products_table

---
+++ database/migrations/2021_12_02_172232_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 500);
+            $table->string('pic', 500);
+            $table->text('content')->nullable();
+            $table->integer('price')->default(0)->unsigned();
+            $table->timestamp('sell_at')->nullable();
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
