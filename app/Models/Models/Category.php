cc216802268a36c1405aba6c79b1c47c4748fea7
colin <colon.song@gmail.com>
2021-12-05 15:15:39 +0800

    category table

---
+++ app/Models/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Models;
+
+use Encore\Admin\Traits\AdminBuilder;
+use Encore\Admin\Traits\ModelTree;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    use ModelTree;
+
+    protected $table="categories";
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setTitleColumn("name");
+    }
+}
