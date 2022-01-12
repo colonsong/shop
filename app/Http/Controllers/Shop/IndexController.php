0696322aaf309e44c0c95ccd7f619eb79c31e37a
colin <colin.song@asiayo.com>
2022-01-12 16:18:06 +0800

    mtest

--- app/Http/Controllers/Shop/IndexController.php
+++ app/Http/Controllers/Shop/IndexController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
-//testmatertest
+//testmatertest2
 class IndexController extends Controller
 {
 
